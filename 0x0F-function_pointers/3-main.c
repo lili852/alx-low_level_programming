5fa72977834c7cb72a64f69d4e2c650b77ba5dc6
lili852 <majedabremli@gmail.com>
2023-05-25 08:16:39 -0700

    task.3*

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,36 +1,46 @@
 #include "3-calc.h"
+
 /**
  * main - Entry point
- * @argc: the number of the parameters
- * @argv: the parameters in the case the number to be calculated.
-(* a blank line
-* Description: this program is the enttry point for a calculator)?
-(* section header: 3-calc.h)*
-* Return: 0 in success
-*/
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
-	int n1, n2, result;
-	int (*p)(int, int);
+	int arg1, arg2, result;
+	char o;
+	int (*func)(int, int);
 
-	if (argc < 4 || argc > 4)
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[3]);
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
 
-	p = get_op_func(argv[2]);
+	func = get_op_func(argv[2]);
 
-	if (p == NULL)
+	if (!func)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	result = p(n1, n2);
+
+	o = *argv[2];
+
+	if ((o == '/' || o == '%') && arg2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = func(arg1, arg2);
 
 	printf("%d\n", result);
+
 	return (0);
 }
