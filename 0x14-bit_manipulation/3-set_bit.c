7a415945d6bf7f7c43d12caefd6b8264e66719bd
lili852 <majedabremli@gmail.com>
2023-06-09 11:22:06 -0700

    set_bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * set_bit - change to 1 the bit of a given index.
+ * @n: the input number
+ * @index: the input index
+ * Return: 1 if works if failure -1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (n == NULL || (index > (sizeof(unsigned long int) * 8) - 1))
+		return (-1);
+
+	*n |= (1 << index);
+	return (1);
+}
